f967f4311d05b45517e6235453e3f205800b1ff4
Turky Gary <turkygary@gmail.com>
2026-03-08 15:45:03 +0300

    update card detection function.

--- misc/scripts/cardDetect.py
+++ misc/scripts/cardDetect.py
@@ -8,25 +8,20 @@ import subprocess
 # Powered by GPIO24
 SensorPin = LED(24)
 SensorPin.on()
-#time.sleep(5)
 
 # Connected to GPIO10 for input digital signal
 sensor = DigitalInputDevice(10, pull_up=False, bounce_time=0.01)
 
 def card_detected():
     print("card detected!")
-#    SensorPin.off()
-#    time.sleep(5)
-#    SensorPin.on()
     subprocess.run(["readIDcard"])
     time.sleep(3)
 
-def path_clear():
-    print("Path is clear.")
+def card_clear():
+    print("Place card to read.")
 
 # Assign functions to events
-#sensor.when_activated = path_clear
-#sensor.when_deactivated = card_detected
+sensor.when_activated = card_clear
 sensor.when_activated = card_detected
 
 print("IR Sensor Active... Press Ctrl+C to exit")

54278f8d062b8bb867504cad735dc232a27cd013
Turky Gary <turkygary@gmail.com>
2026-07-25 15:11:12 +0300

    add script to fetch complete fresh copy from the remote repository on Github

---
+++ git_fetch_remote.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo ppefail
+# -e: stop when a command fails.
+# -u: stop when undefined variable is used.
+# pipefail: detect failure anywhere in a pipeline.
+
+## Fetch remote repo and discard local changes ##
+
+# 1. Fetch the latest metadata and commits from Github without merging
+git fetch origin
+
+# 2. Hard reset current local branch to math the remote branch.
+git reset --hard origin/main
+
+# Clean all untracked files and directories (-f = force, -d = directories)
+git clean -fd
